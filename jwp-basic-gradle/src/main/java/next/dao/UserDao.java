f7bf8645ade54c39ee919f0daa9ac027b7af68e8
HiiWee <wpdlzhf159@gmail.com>
2023-02-12 15:13:34 +0900

    refactor: select문 별도의 SelectJdbcTemplate 이용하도록 추출

--- jwp-basic-gradle/src/main/java/next/dao/UserDao.java
+++ jwp-basic-gradle/src/main/java/next/dao/UserDao.java
@@ -1,15 +1,10 @@
 package next.dao;
 
-import core.jdbc.ConnectionManager;
 import core.jdbc.JdbcTemplate;
-import java.sql.Connection;
+import core.jdbc.SelectJdbcTemplate;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
-import next.exception.DataAccessException;
 import next.model.User;
 
 public class UserDao {
@@ -34,73 +29,33 @@ public class UserDao {
     }
 
     public User findByUserId(String userId) {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, userId);
-
-            rs = pstmt.executeQuery();
-
-            User user = null;
-            if (rs.next()) {
-                user = new User(
-                        rs.getString("userId"),
-                        rs.getString("password"),
-                        rs.getString("name"),
-                        rs.getString("email")
-                );
-            }
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
+        SelectJdbcTemplate selectJdbcTemplate = new SelectJdbcTemplate() {
+            @Override
+            protected String createQuery() {
+                return "SELECT userId, password, name, email FROM USERS WHERE userId = ?";
             }
 
-            return user;
-        } catch (SQLException exception) {
-            throw new DataAccessException(exception.getMessage());
-        }
+            @Override
+            protected void setValue(final PreparedStatement preparedStatement) throws SQLException {
+                preparedStatement.setString(1, userId);
+            }
+        };
+        return selectJdbcTemplate.queryForObject();
     }
 
     public List<User> findAll() {
-        Connection con = null;
-        ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            String sql = "SELECT userId, password, name, email FROM USERS";
-            Statement st = con.createStatement();
+        SelectJdbcTemplate selectJdbcTemplate = new SelectJdbcTemplate() {
+            @Override
+            protected String createQuery() {
+                return "SELECT userId, password, name, email FROM USERS";
+            }
 
-            rs = st.executeQuery(sql);
+            @Override
+            protected void setValue(final PreparedStatement preparedStatement) throws SQLException {
 
-            List<User> users = new ArrayList<>();
-            User user;
-            while (rs.next()) {
-                user = new User(
-                        rs.getString("userId"),
-                        rs.getString("password"),
-                        rs.getString("name"),
-                        rs.getString("email")
-                );
-                users.add(user);
             }
-            if (rs != null) {
-                rs.close();
-            }
-            if (con != null) {
-                con.close();
-            }
-            return users;
-        } catch (SQLException exception) {
-            throw new DataAccessException(exception.getMessage());
-        }
+        };
+        return selectJdbcTemplate.query();
     }
 
     public void update(final User user) {
