53c47cfebb739cedfd7661a7694310f3fbd88e52
HiiWee <wpdlzhf159@gmail.com>
2023-02-12 15:14:16 +0900

    test: 리팩토링한 select문 검증 테스트 추가

--- jwp-basic-gradle/src/test/java/next/dao/UserDaoTest.java
+++ jwp-basic-gradle/src/test/java/next/dao/UserDaoTest.java
@@ -1,20 +1,48 @@
 package next.dao;
 
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
 import next.model.User;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class UserDaoTest {
 
+    private final UserDao userDao = new UserDao();
+
     @Test
+    @Disabled
     public void crud() throws Exception {
         User expected = new User("userId", "password", "name", "javajigi@email.com");
-        UserDao userDao = new UserDao();
         userDao.insert(expected);
 
         User actual = userDao.findByUserId(expected.getUserId());
-        Assertions.assertThat(actual).isEqualTo(expected);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void findByUserId() {
+        // given
+        // when
+        User user = userDao.findByUserId("admin");
+
+        // then
+        assertThat(user).isEqualTo(new User("admin", "password", "자바지기", "admin@slipp.net"));
+    }
+
+    @Test
+    void findAll() {
+        // when
+        List<User> users = userDao.findAll();
+
+        // then
+        Assertions.assertAll(() -> {
+            assertThat(users.size()).isEqualTo(2);
+            assertThat(users.contains(new User("userId", "password", "name", "javajigi@email.com"))).isTrue();
+        });
     }
 
 }
